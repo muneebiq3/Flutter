67dbfafd37ac3712ca82072e35b360ff4aec9425
muneebiq3 <muneebiq526@gmail.com>
2024-11-18 11:55:53 +0500

    Text Theme and folder structure

---
+++ Theme/text_theme/lib/widgets/textheme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Textheme extends StatefulWidget {
+  const Textheme({super.key});
+
+  @override
+  State<Textheme> createState() => _TexthemeState();
+
+}
+
+class _TexthemeState extends State<Textheme> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Theme.of(context).colorScheme.primary,
+            child: Text("sadasdasd", style: Theme.of(context).textTheme.displayLarge)
+          ),
+          Container(
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text("sasd", style: Theme.of(context).textTheme.titleLarge)
+          ),
+          Container(
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text("sad", style: Theme.of(context).textTheme.bodyMedium)
+          ),
+        ],
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme: Theme.of(context).colorScheme.copyWith(secondary: Colors.red),
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.arrow_circle_up),
+        ),
+      ),
+    );
+  }
+}
--- Theme/theme/android/app/src/main/kotlin/com/example/theme/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.theme
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
